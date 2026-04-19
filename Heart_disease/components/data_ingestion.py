b4abfdda44e5169f2038e238629d8726a05875b9
zaidbichu <zaidbichu4@gmail.com>
2026-04-19 07:33:36 -0700

    data ingestion updation completed

--- Heart_disease/components/data_ingestion.py
+++ Heart_disease/components/data_ingestion.py
@@ -55,9 +55,4 @@ class Dataingestion:
             return dataingestion_artifact
         except Exception as e:
             raise heart_disease_exception(e,sys)
-if __name__ == "__main__":
-    training_pipeline_config = trainingpipelineconfig()
-    data_ingestion_config = Dataingestionconfig(training_pipeline_config)
-    data_ingestion = Dataingestion(data_ingestion_config)
-    data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
-    print(data_ingestion_artifact)
+
